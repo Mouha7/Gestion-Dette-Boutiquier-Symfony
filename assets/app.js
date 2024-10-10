5798de5373ffc0c8b839b92241f8fb2d624c80a0
Seydina Mouhamed Rassoul Sow <ra.sow25@gmail.com>
2024-10-10 15:02:51 +0000

    First commit for Symfony

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,22 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
+// Wrong (if bundle not installed)
+// import '@symfony/stimulus-bundle';
+
+// Correct (if bundle is installed)
+// import '@stimulus/stimulus'; // Assuming you're using standalone Stimulus
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// Client configuration
+const menuBurger = document.querySelector("#menu-burger");
+const inputTitle = document.querySelector("#input-title a");
+const sideMenu = document.querySelector("#side-menu");
+
+
+menuBurger.addEventListener("click", () => {
+    sideMenu.classList.toggle("hidden");
+    inputTitle.classList.toggle("hidden");
+});
+
